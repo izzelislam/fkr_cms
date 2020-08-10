bbbc27bda861d54732a566f38f1fdae71cfd35f5
izzelislam <bangfkr002@gmail.com>
2020-08-10 10:38:37 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -98,3 +98,4 @@
         </div>
     </body>
 </html>
+ --}}
