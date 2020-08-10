b00a102b200a6d46203ebb5099b5411823d1e09f
izzelislam <bangfkr002@gmail.com>
2020-08-10 09:51:41 +0700

    make migration

---
+++ database/migrations/2020_08_10_024258_create_article_tag__table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_tag_', function (Blueprint $table) {
+            $table->id();
+            $table->foreignid('article_id');
+            $table->foreignid('tag_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_tag_');
+    }
+}
